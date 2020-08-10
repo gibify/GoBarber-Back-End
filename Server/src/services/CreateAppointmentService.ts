8e55dc5ea32d9c324389473b3b968758675bf142
gibifyOfficial <evandrogibicoski@outlook.com>
2020-08-10 16:56:07 -0300

    gerando hash da senha do usuário

---
+++ Server/src/services/CreateAppointmentService.ts
@@ -0,0 +1,38 @@
+import { startOfHour } from 'date-fns';
+import { getCustomRepository } from 'typeorm';
+
+import Appointment from "../models/Appointment";
+import AppointmentsRepository from '../repositories/AppointmentsRepository';
+
+interface Request {
+    provider_id: string;
+    date: Date;
+};
+
+class CreateAppointmentService {
+    public async execute({ date, provider_id}: Request): Promise<Appointment>{
+        const appointmentsRepository = getCustomRepository(AppointmentsRepository);
+
+        const appointmentDate = startOfHour(date);
+
+
+        const findAppointmentInSamedate = await appointmentsRepository.findByDate(
+            appointmentDate,
+        );
+
+        if(findAppointmentInSamedate) {
+            throw Error('This appointment is alreadt booked');
+        };
+
+        const appointment = appointmentsRepository.create({
+            date: appointmentDate,
+            provider_id,
+    });
+
+        await appointmentsRepository.save(appointment);
+
+        return appointment;
+    }
+};
+
+export default CreateAppointmentService;
