83937660e2a9ccb0f7ee8a489af70963f6c82104
gibifyOfficial <evandrogibicoski@outlook.com>
2020-08-10 23:28:33 -0300

    upload image

--- Server/src/routes/users.routes.ts
+++ Server/src/routes/users.routes.ts
@@ -1,10 +1,15 @@
-import { Router, request, response, json } from 'express';
+import { Router } from 'express';
+import multer from 'multer';
+
+import uploadConfig from '../config/upload';
 
 import CreateUserService from '../services/CreateUserService';
+import UpdateAvatarService from '../services/UpdateUserAvatarService';
 
 import ensureAuthenticated from '../middlewares/ensureAuthenticated';
 
 const usersRouter = Router();
+const upload = multer(uploadConfig);
 
 usersRouter.post('/', async (request, response) => {
    try {
@@ -26,8 +31,22 @@ usersRouter.post('/', async (request, response) => {
     };
 });
 
-usersRouter.patch('/avatar', ensureAuthenticated, async (request, response) => {
-    return response.json({ ok: true });
+usersRouter.patch('/avatar',
+    ensureAuthenticated,
+    upload.single('avatar'),
+    async (request, response) => {
+      try {
+        const updateUserAvatar = new UpdateAvatarService();
+
+        updateUserAvatar.execute({
+            user_id: request.user.id,
+            avatarFilename: request.file.filename,
+        });
+
+        return response.json({ ok: true })
+      } catch (err) {
+        return response.status(400).json({ error: err.message });
+      }
 });
 
 export default usersRouter;
