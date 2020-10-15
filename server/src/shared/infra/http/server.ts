2ceeed561bdd92218b63cf50d95ea4e0531d0b65
gibify <evandrogibicoski@outlook.com>
2020-10-15 09:58:36 -0300

    create connections to mongodb

--- server/src/shared/infra/http/server.ts
+++ server/src/shared/infra/http/server.ts
@@ -4,6 +4,7 @@ import express, { Request, Response, NextFunction } from 'express';
 import cors from 'cors';
 import 'express-async-errors';
 
+import { errors } from 'celebrate';
 import routes from './routes';
 import uploadConfig from '@config/upload';
 import AppError from '@shared/errors/AppError';
@@ -17,6 +18,9 @@ app.use(cors());
 app.use(express.json());
 app.use('/file', express.static(uploadConfig.uploadsFolder));
 app.use(routes);
+
+app.use(errors());
+
 app.use((err: Error, request: Request, response: Response, next: NextFunction) => {
     if(err instanceof AppError) {
         return response.status(err.statusCode).json({
